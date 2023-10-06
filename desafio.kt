32bb4c465a2316707a7e54b7d56c16b8e1ec448d
Nicolas Jandre <46204346+nicolasjandre@users.noreply.github.com>
2023-10-06 12:06:09 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,42 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        if (!inscritos.contains(usuario)) {
+            inscritos.add(usuario)
+            println("Matrícula realizada com sucesso para ${usuario.nome} na formação $nome.")
+        } else {
+            println("${usuario.nome} já está matriculado na formação $nome.")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    // Criando alguns usuários
+    val usuario1 = Usuario("Alice")
+    val usuario2 = Usuario("Bob")
+
+    // Criando alguns conteúdos educacionais
+    val conteudo1 = ConteudoEducacional("Introdução à Programação", 90)
+    val conteudo2 = ConteudoEducacional("Algoritmos Avançados", 120)
+    val conteudo3 = ConteudoEducacional("Estruturas de Dados", 90)
+
+    // Criando uma formação
+    val formacao = Formacao("Ciência da Computação", listOf(conteudo1, conteudo2, conteudo3))
+
+    // Matriculando usuários na formação
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
+    formacao.matricular(usuario1) // Tentando matricular o mesmo usuário novamente
+
+    // Exibindo inscritos na formação
+    println("Inscritos na formação ${formacao.nome}: ${formacao.inscritos.map { it.nome }}")
 }
